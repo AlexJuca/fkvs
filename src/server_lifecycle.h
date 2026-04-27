4335d9394625c3f3cf424874eeaaaf0f52b33b10
Alexandre Antonio Juca <corextechnologies@gmail.com>
2026-04-27 22:18:44 +0100

    fix: centralize client drop cleanup

--- src/server_lifecycle.h
+++ src/server_lifecycle.h
@@ -5,9 +5,12 @@
 
 #include <stdbool.h>
 
+typedef struct client_t client_t;
+
 void request_server_shutdown(int sig);
 bool server_shutdown_requested(void);
 void reset_server_shutdown_request(void);
+void server_drop_client(server_t *srv, client_t *client);
 void shutdown_server(server_t *srv);
 
 #endif // SERVER_LIFECYCLE_H
